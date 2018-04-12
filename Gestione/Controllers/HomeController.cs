3f8063a63ac60b9badfea62ee7a520f1b00541c0
mtani93 <matteo.cecina@gmail.com>
2018-04-12 16:40:47 +0200

    rg

--- Gestione/Controllers/HomeController.cs
+++ Gestione/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Interfaces;
+using Gestione.Models;
 
 namespace Gestione.Controllers {
     public class HomeController : Controller {
@@ -21,5 +23,20 @@ namespace Gestione.Controllers {
 
             return View();
         }
+		 public ActionResult Corso(int id) {
+			DomainModel dm = new DomainModel();
+			Interfaces.Corso scelto = dm.SearchCorsi(id);
+			List<Lezione> lezions = new List<Lezione>();
+			foreach(Lezione l in scelto.Lezioni){
+			lezions.Add(l);
+			}
+			ViewBag.Lezioni = lezions;
+           return View();
+        }
+		public ActionResult Iscrizione(int idCorso,int idStudente){
+			DomainModel dm = new DomainModel();
+			dm.Iscriviti(idCorso,idStudente);
+			return View();
+		}
     }
 }
