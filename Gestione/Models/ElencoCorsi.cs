af00ecd7ce361f978ad1e4c4f35e113f8586ede1
brolin2 <gheliuc.florin@gmail.com>
2018-04-12 15:26:56 +0200

    Aggiuta Pagine e metodoModello

--- Gestione/Models/ElencoCorsi.cs
+++ Gestione/Models/ElencoCorsi.cs
@@ -5,9 +5,9 @@ using Interfaces;
 
 namespace Gestione.Models{
     partial class DomainModel:IGeCo,IGeCV,IGeTime{
-	public List<Corso> ListaCorsi(string idUtente){
+		public List<Corso> ListaCorsi(string idUtente){
 			DataAccessObject dto = new DataAccessObject();
-			List<Corso> result = ()dto.FindCorso(idUtente);
+			List<Corso> result = dto.FindCorso(idUtente);
 			return result;
 		}
     }
