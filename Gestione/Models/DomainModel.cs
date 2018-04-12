d87e40ae518760cdb86dab5a4aceb481f4a08e41
brolin2 <gheliuc.florin@gmail.com>
2018-04-12 15:29:12 +0200

    Revert "Aggiuta Pagine e metodoModello"
    This reverts commit af00ecd7ce361f978ad1e4c4f35e113f8586ede1.

--- Gestione/Models/DomainModel.cs
+++ Gestione/Models/DomainModel.cs
@@ -5,10 +5,5 @@ using Interfaces;
 
 namespace Gestione.Models{
     partial class DomainModel:IGeCo,IGeCV,IGeTime{
-	public List<Corso> ListaCorsi(string idUtente){
-			DataAccessObject dto = new DataAccessObject();
-			List<Corso> result = ()dto.FindCorso(idUtente);
-			return result;
-		}
     }
 }
