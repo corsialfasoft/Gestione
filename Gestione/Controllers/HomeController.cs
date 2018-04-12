750d2c4df6075002616f880036c4b85984ec960d
TandyD <brinzila30@gmail.com>
2018-04-12 13:56:15 +0200

    t

--- Gestione/Controllers/HomeController.cs
+++ Gestione/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 namespace Gestione.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
-            return View();
+            return View ();
         }
 
         public ActionResult About() {
