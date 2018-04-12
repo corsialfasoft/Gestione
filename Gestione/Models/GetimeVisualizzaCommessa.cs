4dfc6bb2980ea16408137a94b431b75d1f21c40d
naumanaziz <aziznauman96@hotmail.it>
2018-04-12 15:30:20 +0200

    n

---
+++ Gestione/Models/GetimeVisualizzaCommessa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Interfaces;
+
+namespace Gestione.Models {
+	public partial class DomainModel {
+		public List<Giorno> GiorniCommessa(int idCommessa, int idUtente){
+			
+		}
+		public Commessa CercaCommessa(string nomeCommessa) {
+			
+		}
+	}
+
+}
