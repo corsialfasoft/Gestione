68a2838de3c6944072bdbaf03facbf1a65bf8d05
TandyD <brinzila30@gmail.com>
2018-04-13 09:15:59 +0200

    inizio di dao

--- Gestione/Models/GeCoAddCorso.cs
+++ Gestione/Models/GeCoAddCorso.cs
@@ -3,7 +3,12 @@
 namespace Gestione.Models {
     partial class DomainModel:IGeCo,IGeCV,IGeTime{
         public void AddCorso(Corso corso){ 
-                
+              /*
+               db.AddCorso(corso);
+                using (NomeEntity db = new NomeEntity()){
+                    
+                }  
+             */  
         }
     }
 }
