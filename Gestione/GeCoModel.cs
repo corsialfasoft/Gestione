a81912ee56b167b18c2023dbefa79939b7489f46
mtani93 <matteo.cecina@gmail.com>
2018-04-14 21:25:13 +0200

    grafica corso

---
+++ Gestione/GeCoModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Gestione {
+	using System;
+	using System.Data.Entity;
+	using System.Linq;
+
+	public class GeCoModel : DbContext {
+		// Il contesto è stato configurato per utilizzare una stringa di connessione 'GeCoModel' dal file di configurazione 
+		// dell'applicazione (App.config o Web.config). Per impostazione predefinita, la stringa di connessione è destinata al 
+		// database 'Gestione.GeCoModel' nell'istanza di LocalDb. 
+		// 
+		// Per destinarla a un database o un provider di database differente, modificare la stringa di connessione 'GeCoModel' 
+		// nel file di configurazione dell'applicazione.
+		public GeCoModel()
+			: base("name=GeCoModel") {
+		}
+
+		// Aggiungere DbSet per ogni tipo di entità che si desidera includere nel modello. Per ulteriori informazioni 
+		// sulla configurazione e sull'utilizzo di un modello Code, vedere http://go.microsoft.com/fwlink/?LinkId=390109.
+
+		// public virtual DbSet<MyEntity> MyEntities { get; set; }
+	}
+
+	//public class MyEntity
+	//{
+	//    public int Id { get; set; }
+	//    public string Name { get; set; }
+	//}
+}
