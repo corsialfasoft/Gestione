46b6c4bbb4518a3c11d6892fd8641a27025f3596
TandyD <brinzila30@gmail.com>
2018-04-16 09:52:35 +0200

    ora ce veramnte

---
+++ Gestione/DataBase/GeCo.sql
@@ -0,0 +1,27 @@
+﻿create table GeCorsi;
+
+create table Corsi(
+	id int identity(1,1) primary key not null,
+	nome nvarchar(50) not null,
+	descrizione nvarchar (200),
+	dInizio date,
+	dFine date
+);
+
+create table Lezioni (
+	id int identity(1,1) primary key not null,
+	durata nvarchar(50) not null,
+	idCorsi int foreign key references Corsi
+);
+
+create table Studenti(
+	matr nvarchar(10) primary key not null,
+	nome varchar (15) not null,
+	cognome varchar(15) not null
+);
+
+create table StudentiCorsi(
+	idCorso int foreign key references Corso,
+	idStudenti int foreign key references Studenti,
+	primary key(idCorso,idStudenti)
+);
