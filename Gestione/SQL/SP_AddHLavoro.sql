3e0e32bf1a1b3461b5857955f37a94c3cc9ac86c
LukeGent <lucgent@gmail.com>
2018-04-18 15:05:44 +0200

    Fix SP_AddHLavoro

--- Gestione/SQL/SP_AddHLavoro.sql
+++ Gestione/SQL/SP_AddHLavoro.sql
@@ -1,41 +1,56 @@
 create procedure SP_AddHLavoro
-	--DateTime data, int ore, int idCommessa, string idUtente
-
-	--idGiorno int foreign key references Giorni not null,
-	--idCommessa int foreign key references Commesse not null,
-	--ore int not null,
-	--primary key(idGiorno,idCommessa)
-
 	@data Date,
 	@ore int,
 	@idCommessa int,
 	@idUtente as nvarchar(10)
 as
 	DECLARE @idGiorno int = (
-	SELECT TOP 1 id FROM Giorni
+	SELECT id FROM Giorni
 	WHERE giorno=@data AND idUtente=@idUtente);
 
 	IF @idGiorno IS NULL
 		BEGIN
 			INSERT INTO Giorni (giorno, idUtente) VALUES (@data, @idUtente);
-			SET @idGiorno = (SELECT IDENT_CURRENT ('Giorni'))
+			SET @idGiorno = (SELECT IDENT_CURRENT ('Giorni'));
 		end;
-		declare @oreNonLav int = 0;
-		set @oreNonLav = (select sum(ore) from OreNonLavorative where idGiorno = @idGiorno);
 
-		declare @oreLav int = 0;
-		set @oreLav = (select sum(ore) from OreLavorative where idGiorno = @idGiorno);
-			
-		if (@ore + @oreLav + @oreNonLav <= 8)
-			begin
+	declare @oreNonLav int;
+	set @oreNonLav = (select sum(ore) from OreNonLavorative where idGiorno = @idGiorno);
+
+	declare @oreLav int;
+	set @oreLav = (select sum(ore) from OreLavorative where idGiorno = @idGiorno);
+
+	if (@oreNonLav is null)
+		begin
+			set @oreNonLav = 0;
+		end
+
+	if (@oreLav is null)
+		begin
+			set @oreLav = 0;
+		end
+	
+	if (@ore + @oreLav + @oreNonLav <= 8)
+		begin
+			begin try
 				insert into OreLavorative(idGiorno, idCommessa, ore) values(@idGiorno, @idCommessa, @ore);
-				if(@@Error >0)
+				return 1;
+			end try
+			begin catch			
+				if(@@Error = 2627)
 					begin
-						declare @oreDellaComm int = (select ore from OreLavorative where idGiorno=@idGiorno and idCommessa = @idCommessa);
+						declare @oreDellaComm int = (select top 1 ore from OreLavorative where idGiorno=@idGiorno and idCommessa = @idCommessa);
 						update OreLavorative set ore=@ore+@oreDellaComm where idGiorno=@idGiorno and idCommessa = @idCommessa;
 					end
-			end;
-		else
+			end catch
+		end
+	else
+		begin
 			return 0;
+		end
 	
 go
+
+exec SP_AddHLavoro "2018/04/8", 1, 1, "n.1";
+
+select * from OreLavorative
