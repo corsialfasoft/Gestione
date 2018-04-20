6ac211871918544be3691b6b2485f12009be778a
Alessandro Fabiani <alxssandro@hotmail.it>
2018-04-20 11:20:53 +0100

    TestVisualizza commessa TestVisualizza giorno

--- Gestione.Tests/Controllers/HomeControllerTest.cs
+++ Gestione.Tests/Controllers/HomeControllerTest.cs
@@ -49,7 +49,21 @@ namespace Gestione.Tests.Controllers {
 		public void VisualizzaCommessa() {
 			HomeController controller = new HomeController();
 			ViewResult result = controller.VisualizzaCommessa("GeTime") as ViewResult;
-			Assert.IsTrue(result.ViewBag.Giorni.Count==3);
+			Assert.IsNotNull(result.ViewBag.Giorni);
 		}
-	}
+        [TestMethod]
+        public void VisualizzaGiorno() {
+           
+            HomeController controller = new HomeController();
+            string value = "2000-01-01"; //non funziona se nn sono presenti nel DB questi valori!!!!!!!!!
+            DateTime convertedDate ;
+            convertedDate = Convert.ToDateTime(value); //non funziona se nn sono presenti nel DB questi valori!!!!!!!!!
+            ViewResult result = controller.VisualizzaGiorno(convertedDate) as ViewResult;
+            
+            Assert.IsTrue(result.ViewBag.giorno.data == convertedDate);
+            Assert.IsTrue(result.ViewBag.giorno.OreLavorate [0].oreGiorno == 4); //non funziona se nn sono presenti nel DB questi valori!!!!!!!!!
+            Assert.IsTrue(result.ViewBag.giorno.TotOreLavorate == 4); //non funziona se nn sono presenti nel DB questi valori!!!!!!!!!
+            Assert.IsTrue(result.ViewBag.giorno.OreMalattia == 0); //non funziona se nn sono presenti nel DB questi valori!!!!!!!!!
+        }
+    }
 }
