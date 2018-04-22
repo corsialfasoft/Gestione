469851c1aa5686afc7315e7a933add8732776d4e
mtani93 <matteo.cecina@gmail.com>
2018-04-22 17:00:47 +0200

    try-catch delegate

---
+++ Gestione/try.cs
@@ -0,0 +1,10 @@
+﻿public delegate TOutput DaoCall<TInput, TOutput>(TInput input );
+		public TOutput Try<TInput, TOutput>(DaoCall<TInput, TOutput> prova, TInput k) {
+			try{
+				
+				TOutput res =  prova(k);
+				return res;
+			}catch(Exception e){
+				throw e;
+			}
+		}
