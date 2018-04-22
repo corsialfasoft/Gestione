6cd12f75962d5f58b7db101c622e3152ada3cb3e
naumanaziz <aziznauman96@hotmail.it>
2018-04-22 19:07:21 +0200

    link

---
+++ Gestione/Models/link.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gestione.Models {
+    public class link {
+    }
+}
+https://sourcemaking.com/design_patterns/command/java/2
