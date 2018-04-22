e6043484d9c876acbb1d23e1ea5f07858e3bc06c
mtani93 <matteo.cecina@gmail.com>
2018-04-22 17:46:31 +0200

    overload void

--- Gestione/try.cs
+++ Gestione/try.cs
@@ -14,7 +14,14 @@
 using System;
 namespace tre{
 public class doh{
-public delegate void DaoCall<TInput>(TInput input);
+public delegate void DaoCall<TInput1>(TInput1 input);
+public delegate void DaoCall<TInput1,TInput2>(TInput1 input1, TInput2 input2);
+public delegate void DaoCall<TInput1,TInput2, TInput3>(TInput1 input1, TInput2 input2, TInput3 input3);
+public delegate void DaoCall<TInput1,TInput2, TInput3, TInput4>(TInput1 input1, TInput2 input2, TInput3 input3, TInput4 input4);
+public delegate void DaoCall<TInput1,TInput2, TInput3, TInput4, TInput5>(TInput1 input1, TInput2 input2, TInput3 input3, TInput4 input4, TInput5 input5);
+public delegate void DaoCall<TInput1,TInput2, TInput3, TInput4, TInput5, TInput6>(TInput1 input1, TInput2 input2, TInput3 input3, TInput4 input4, TInput5 input5, TInput6 input6);
+public delegate void DaoCall<TInput1,TInput2, TInput3, TInput4, TInput5, TInput6, TInput7>(TInput1 input1, TInput2 input2, TInput3 input3, TInput4 input4, TInput5 input5, TInput6 input6, TInput7 input7);
+
 public delegate TOutput DaoCall<TInputa,TInputb, TOutput>(TInputa inputa, TInputb inputb);
 public delegate TOutput DaoCall<TInput, TOutput>(TInput input );
 		public TOutput Try<TInputa,TInputb, TOutput>(DaoCall<TInputa,TInputb, TOutput> prova, TInputa k, TInputb h) {
