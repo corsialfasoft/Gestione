9129c1c8fad56a5e35ce560350a74809236ccb27
naumanaziz <aziznauman96@hotmail.it>
2018-04-23 21:02:38 +0200

    aggiornato tabella giorni e fattorizzato

--- Gestione/Controllers/GeTimeDettaglioMese.cs
+++ Gestione/Controllers/GeTimeDettaglioMese.cs
@@ -8,8 +8,12 @@ namespace Gestione.Controllers {
     public partial class HomeController{
         [HttpPost]
         public ViewResult VisualizzaMese(string anno,string mese) { 
-            if(anno !="" && mese !="") {
-               ViewBag.Mese = dm.DettaglioMese(int.Parse(anno),int.Parse(mese), P.Matricola);
+            if(int.TryParse(anno,out int annoI) && int.TryParse(mese,out int meseI)) {
+               ViewBag.Mese = dm.DettaglioMese(annoI,meseI, P.Matricola);
+                if (ViewBag.Mese.Count > 0) {
+                    @ViewBag.Year = annoI;
+                    ViewBag.Month = meseI;
+                }
             } else
                 ViewBag.Message ="Inserire anno e mese";
             return View();
