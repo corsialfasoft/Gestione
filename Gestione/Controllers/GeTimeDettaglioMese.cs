c6c0cd75ef6762e2f5ae2a0a128a0c1c349f6d5b
naumanaziz <aziznauman96@hotmail.it>
2018-04-23 22:01:10 +0200

    fine implementazione GeTime tabelle

--- Gestione/Controllers/GeTimeDettaglioMese.cs
+++ Gestione/Controllers/GeTimeDettaglioMese.cs
@@ -11,8 +11,12 @@ namespace Gestione.Controllers {
             if(int.TryParse(anno,out int annoI) && int.TryParse(mese,out int meseI)) {
                ViewBag.Mese = dm.DettaglioMese(annoI,meseI, P.Matricola);
                 if (ViewBag.Mese.Count > 0) {
-                    @ViewBag.Year = annoI;
+                    ViewBag.Year = annoI;
                     ViewBag.Month = meseI;
+                    ViewBag.TOreL = ((List<DTGiornoDMese>)ViewBag.Mese).Sum<DTGiornoDMese>(dTGiornoDMese => dTGiornoDMese.TotOreLavorate);
+                    ViewBag.TOreP = ((List<DTGiornoDMese>)ViewBag.Mese).Sum<DTGiornoDMese>(dTGiornoDMese => dTGiornoDMese.OrePermesso);
+                    ViewBag.TOreM = ((List<DTGiornoDMese>)ViewBag.Mese).Sum<DTGiornoDMese>(dTGiornoDMese => dTGiornoDMese.OreMalattia);
+                    ViewBag.TOreF = ((List<DTGiornoDMese>)ViewBag.Mese).Sum<DTGiornoDMese>(dTGiornoDMese => dTGiornoDMese.OreFerie);
                 }
             } else
                 ViewBag.Message ="Inserire anno e mese";
