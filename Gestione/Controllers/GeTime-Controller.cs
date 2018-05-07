b8f1bf899ec0d61b345aa17a9addc79016ee455a
naumanaziz <aziznauman96@hotmail.it>
2018-05-07 07:40:32 +0200

    modifiche

--- Gestione/Controllers/GeTime-Controller.cs
+++ Gestione/Controllers/GeTime-Controller.cs
@@ -203,5 +203,32 @@ namespace Gestione.Controllers {
             public DateTime Data { get; set; }
             public int Ore { get; set; }
         }
+        public ActionResult AddCommessa() {
+            return View("AddCommessa");
+        }
+        [HttpPost]
+        public ActionResult AddCommessa(string commessa, string descrCommessa, string stimaOre) {
+            try {
+                int oreStimate = 0;
+                bool oreBool = int.TryParse(stimaOre, out oreStimate);
+                if (commessa != null && descrCommessa != null && oreBool != false && oreStimate > 0) {
+                    DTCommessa comm = dm.CercaCommessa(commessa);
+                    if (comm == null) {
+                        comm = new DTCommessa();
+                        comm.Nome = commessa;
+                        comm.Descrizione = descrCommessa;
+                        comm.Capienza = oreStimate;
+                        dm.AddCommessa(comm);
+                        ViewBag.AddCommessa = comm;
+                    } else {
+                        ViewBag.Mex = "Operazione non consentita, commessa già esistente";
+                    }
+                } else
+                    ViewBag.Mex = "Operazione non consentita, campi non riempiti correttamente";
+            } catch (Exception e) {
+                ViewBag.Mex = e.Message;
+            }
+            return View("AddCommessa");
+        }
     }
 }
