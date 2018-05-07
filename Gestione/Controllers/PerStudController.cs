a850f383c489597ef5ab54d959a0b768e5273938
emmeacs <massimo.franzoso8895@gmail.com>
2018-05-07 10:17:07 +0200

    Aggiuto PerStud controller

---
+++ Gestione/Controllers/PerStudController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Interfaces;
+using Gestione.Models;
+
+namespace Gestione.Controllers
+{
+	public class PerStudController : ApiController
+	{
+		DomainModel dm = new DomainModel();
+		// GET api/<controller>
+		[HttpGet][Route("api/Cv/{idCV}/PerStud/")]
+		public IEnumerable<PerStud> Get()
+		{
+			return null;
+		}
+
+		// GET api/<controller>/5
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<controller>
+		public void Post([FromBody]string value)
+		{
+		}
+
+		// PUT api/<controller>/5
+		public void Put(int id,[FromBody]string value)
+		{
+		}
+
+		// DELETE api/<controller>/5
+		public void Delete(int id)
+		{
+		}
+	}
+}
