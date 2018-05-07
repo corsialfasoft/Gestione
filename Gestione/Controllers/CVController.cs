ff5880454a5f0aac9d8a5cca6acfbd58c828d1b3
brolin2 <gheliuc.florin@gmail.com>
2018-05-07 10:03:38 +0200

    aggiunto controller cv

---
+++ Gestione/Controllers/CVController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Interfaces;
+using Gestione.Models;
+
+namespace Gestione.Controllers{
+    public class CVController : ApiController{
+		DomainModel dm = new DomainModel();
+
+		public List<CV> Get(){
+			 return dm.SearchChiava("");
+		}
+		public CV Get(String id){
+			return dm.Search(id);
+		}
+		public void Post([FromBody]CV cv) {
+            dm.AggiungiCV(cv);
+        }
+		public void Delete(string id ){
+			dm.EliminaCV(dm.Search(id));
+		}
+    }
+}
