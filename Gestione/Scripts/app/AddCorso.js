ca4db7cef187fa3025b33efc2c404eb050744f4a
lucasalzone <luca.salzone@gmail.com>
2018-05-08 09:32:10 +0200

    Component Client Example with jquery (load)

---
+++ Gestione/Scripts/app/AddCorso.js
@@ -0,0 +1,15 @@
+﻿$("#frmNuovo").submit(function (event) {
+    event.preventDefault();
+    var $form = $(this);
+    var corso = {};
+    corso.Descrizione = $form.find("textarea[name='_descrizione']").val();
+    corso.Inizio = $form.find("input[name='_inizio']").val();
+    corso.Fine = $form.find("input[name='_fine']").val();
+    corso.Nome = $form.find("input[name='_nome']").val();
+    var posting = $.post($form.attr("action"), corso);
+    posting.done(function (data) {
+        //var content = $(data).find("#Message");
+        $(document).trigger("CorsoAdded", data);
+        $("#Message").append(data);
+    });
+});
---
+++ Gestione/Scripts/app/DettaglioCorso.js
@@ -0,0 +1,24 @@
+﻿function LoadCorso(id,element) {
+    $.getJSON(uri + '/' + id)
+        .done(function (data) {
+            $(element).html(
+                '<div class="panel panel-primary">' +
+                '<div class="panel-heading">' + data.Nome + '</div>' +
+                '<div class="panel-body"> <p><small>' + data.Descrizione + '</small></p>' +
+                '<div class="form-group"><label>Data Inizio</label> ' +
+                '<input disabled type="text" class="form-control" value="' + data.Inizio + '">' +
+                '<div class="form-group"><label>Data Fine</label> ' +
+                '<input disabled type="text" class="form-control" value="' + data.Fine + '"></div>' +
+
+                '<a class="btn btn-success" href="#" role="button">Modifica</a> &nbsp;' +
+                '<a class="btn btn-warning" href="#" role="button">Elimina</a>' +
+                '</div>' +
+                '</div > ');
+            $('#Lezioni').empty();
+            $('#Lezioni').append("<ul class='list-group' >");
+            $.each(data.Lezioni, function (key, item) {
+                $('#Lezioni').append("<li class='list-group-item'>" + item.Nome + "</li>");
+            });
+            $('#Lezioni').append("</ul>");
+        });
+}
